cbc1da61367dc417d377e2493ef44beb9e5ef3b8
sikwan <yysskk99999@naver.com>
2023-12-31 14:34:08 +0900

    add

--- FastSwiftBasic/FastSwiftBasic/VariableType.swift
+++ FastSwiftBasic/FastSwiftBasic/VariableType.swift
@@ -12,12 +12,19 @@ struct VariableType: View {
     var name: String = "Leo"
     var age: Int = 20
     var height : Float = 183.6
+    var weight: Double = 70.6
     var havePet: Bool = false
         
     
     
     var body: some View {
-        Text("10")
+        VStack{
+            Text("\(name)")
+            Text("\(age)")
+            Text("\(height)")
+            Text("\(weight)")
+            Text("\(havePet.description)")
+        }
     }
 }
 
