4f77385afc02c0b168bb2382a8d5c7fa3525f3b1
sikwan <yysskk99999@naver.com>
2024-01-01 15:05:36 +0900

    observerved think

--- FastSwiftBasic/FastSwiftBasic/Diff.swift
+++ FastSwiftBasic/FastSwiftBasic/Diff.swift
@@ -9,15 +9,24 @@ import SwiftUI
 
 struct Diff: View {
     let mycar = Car(name: "리어카", owner: "리이오")
-    let maKar = Kar(name: "리어카2", owner: "리이오2")
-    
+   @ObservedObject var myKar = Kar(name: "리어카2", owner: "리이오2")
+    @State var name : String = ""
     
     var body: some View {
         VStack{
-            Text("\(mycar.name)의 주인은 \(maKar.owner)")
+            Text("\(mycar.name)의 주인은 \(myKar.owner)")
+            
+            TextField("Placeholder", text: $name)
             
             Button{
+                
+                var broCar = mycar
+                broCar.name = "동생차"
+                broCar.owner = "동생"
+                
+                
                 mycar.sayHi()
+                broCar.sayHi()
             }
         label: {
                 Text("출발")
@@ -27,17 +36,17 @@ struct Diff: View {
 }
 
 struct Car{
-    let name :String
-    let owner : String
+    var name :String
+    var owner : String
     
     func sayHi(){
         print("hi \(owner)")
     }
 }
 
-class Kar{
-    let name : String
-    let owner : String
+class Kar : ObservableObject{
+    @Published var name: String
+    var owner : String
     
     func sayHi(){
         print("hi \(owner)")
