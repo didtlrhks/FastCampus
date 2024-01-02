560ee9273b7c0aed54ffd548c33b3fefbaeb6329
sikwan <yysskk99999@naver.com>
2024-01-02 17:24:19 +0900

    board ex

---
+++ FastSwiftBasic/FastSwiftBasic/EMXApp.swift
@@ -0,0 +1,9 @@
+//
+//  EMXApp.swift
+//  FastSwiftBasic
+//
+//  Created by 양시관 on 1/2/24.
+//
+
+import Foundation
+
---
+++ FastSwiftBasic/FastSwiftBasic/Exapp.swift
@@ -0,0 +1,47 @@
+//
+//  SwiftUIView.swift
+//  FastSwiftBasic
+//
+//  Created by 양시관 on 1/2/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack{
+            Image(systemName : "pencil")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200,height: 200)
+            
+            Text("헤드라인입니다.")
+                .font(.headline)
+                .bold()
+                .padding()
+            
+            Text("서브 헤드라인입니다.")
+                .font(.subheadline)
+                .padding()
+            
+            Text("바디")
+                .font(.body)
+                .padding()
+            
+            
+            Button {
+                
+            }label: {
+                Text("Click me!")
+                    .padding()
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(24)
+            }
+        }
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
