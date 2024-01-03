c5169681346455bfd3b511a59023e0f495d9c909
sikwan <yysskk99999@naver.com>
2024-01-03 22:27:17 +0900

    List Exercise 2

--- FastSwiftBasic/FastSwiftBasic/ListLoop.swift
+++ FastSwiftBasic/FastSwiftBasic/ListLoop.swift
@@ -10,19 +10,19 @@ import SwiftUI
 
 struct Fruit : Hashable {
     let name : String
-    let favorite : String
+    let matchFavoriteName : String
     let price : Int
 }
 
 struct ListLoop: View {
-    var favoriteFruits = [
+    @State var favoriteFruits = [
     
-        Fruit(name:"apple",favorite:"Banana",price:1000),
-        Fruit(name: "Banana", favorite: "Banana", price: 2000),
-        Fruit(name:"apple",favorite:"kiwi",price:3000),
-        Fruit(name:"Double Kiwi",favorite:"berry",price:4000),
-        Fruit(name:"apple",favorite:"cherry",price:7000),
-        Fruit(name:"Apple",favorite:"Banana",price:1000)
+        Fruit(name:"apple",matchFavoriteName:"Banana",price:1000),
+        Fruit(name: "Banana", matchFavoriteName: "Banana", price: 2000),
+        Fruit(name:"apple",matchFavoriteName:"kiwi",price:3000),
+        Fruit(name:"Double Kiwi",matchFavoriteName:"berry",price:4000),
+        Fruit(name:"apple",matchFavoriteName:"cherry",price:7000),
+        Fruit(name:"Apple",matchFavoriteName:"Banana",price:1000)
     
     ]
     
@@ -30,29 +30,51 @@ struct ListLoop: View {
     var count = 0
     
     
+    @State var fruitName: String = ""
     var body: some View {
         NavigationStack{
-            List {
-                ForEach(favoriteFruits, id : \.self){
-                    fruit in
-                    VStack(alignment: .leading)
-                    {
-                        Text("name : \(fruit.name)")
-                        Text("name : \(fruit.favorite)")
-                        Text("name : \(fruit.price)")
-                        Text("name : \(fruit.name)")
-                        Text("name : \(fruit.name)")
-                        Text("name : \(fruit.name)")
-                        
-                     
-                    }
-                   
-                }
-                   
-                    
+            
+            VStack{
                 
+                HStack{
+                    TextField("insert fruit name", text: $fruitName)
+                    Button{
+                        favoriteFruits.append(Fruit(name: fruitName, matchFavoriteName: "Apple", price: 1000))
+                        
+                    }label: {
+                        Text("insert")
+                            .padding()
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+                .padding()
+                List {
+                    ForEach(favoriteFruits, id : \.self){
+                        fruit in
+                        VStack(alignment: .leading)
+                        {
+                            Text("name : \(fruit.name)")
+                            Text("matchFruitName : \(fruit.matchFavoriteName)")
+                            Text("price : \(fruit.price)")
+//                            Text("name : \(fruit.name)")
+//                            Text("name : \(fruit.name)")
+//                            Text("name : \(fruit.name)")
+//                            
+                            
+                        }
+                        
+                    }.onDelete{
+                        indexSet in
+                        favoriteFruits.remove(atOffsets: indexSet)
+                    }
+                    
+                    
+                    
+                }
+                .navigationTitle("Fruit List")
             }
-            .navigationTitle("Fruit List")
         }
     }
 }
